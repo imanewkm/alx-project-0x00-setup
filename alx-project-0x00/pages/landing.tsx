43d07f646d426fe17f5cebf7554e2844040dedb5
imanewkm <imanewkm02@gmail.com>
2025-05-31 23:32:05 +0100

    rounded-lg

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,7 +7,7 @@ const Landing: React.FC =  () => {
       <Card />
       <Card />
       <Button title="Get Started" size="large" shape="rounded-sm" className="mt-4" onClick={() => alert('Button Clicked!')} />
-      <Button title="Learn More" size="medium" shape="rounded-" className="mt-2" onClick={() => alert('Learn More Clicked!')} />
+      <Button title="Learn More" size="medium" shape="rounded-lg" className="mt-2" onClick={() => alert('Learn More Clicked!')} />
       <Button title="Contact Us" size="small" shape="rounded-full" className="mt-2" onClick={() => alert('Contact Us Clicked!')} />
       <Button title="Sign Up" size="large" shape="rounded-md" className="mt-2" onClick={() => alert('Sign Up Clicked!')} /> 
     </div>
