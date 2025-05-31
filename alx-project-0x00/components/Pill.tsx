05d4d9256839315a6ac9d3ba740e3b31aec29cb9
imanewkm <imanewkm02@gmail.com>
2025-05-31 22:50:47 +0100

    card.tsx

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,1 +1,15 @@
+import React from "react"
 
+interface PillProps {
+  title: string;
+}
+
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className="pill">
+      {title}
+    </div>
+  )
+}
+
+export default Pill
