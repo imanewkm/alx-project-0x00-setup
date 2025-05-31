142e9637aa34782526752afa7bd299aa4abd5b74
imanewkm <imanewkm02@gmail.com>
2025-05-31 23:30:23 +0100

    Button file name correction

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,7 +6,8 @@ const Landing: React.FC =  () => {
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
       <Card />
-      <Button title="Get Started" size="large" shape="rounded-full" className="mt-4" onClick={() => alert('Button Clicked!')} />
+      <Button title="Get Started" size="large" shape="rounded-sm" className="mt-4" onClick={() => alert('Button Clicked!')} />
+      <Button title="Learn More" size="medium" shape="rounded-md" className="mt-2" onClick={() => alert('Learn More Clicked!')} />
     </div>
   )
 }
